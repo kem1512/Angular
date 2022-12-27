f605b26953ff2ce814d050c429513990d94cbe6f
hoan <kem15122002@gmail.com>
2022-12-27 17:22:55 +0700

    ok

--- todolist/src/app/taskform/taskform.component.ts
+++ todolist/src/app/taskform/taskform.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http'
 import { TasklistComponent } from '../tasklist/tasklist.component';
-import { ITask } from '../entities/task';
 
 @Component({
   selector: 'app-taskform',
@@ -12,17 +10,13 @@ import { ITask } from '../entities/task';
 export class TaskformComponent implements OnInit {
   @ViewChild('child') child!:TasklistComponent;
 
-  url = 'https://62a0a59da9866630f8145222.mockapi.io/api/tasklist/'
-
-  @ViewChild('f') taskform!: NgForm
-
   constructor(private httService: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   addTask() {
-    this.httService.post(this.url, this.child.task).subscribe(c => {
+    this.httService.post(this.child.url, this.child.task).subscribe(c => {
       this.child.getAllTask()
     }, error => {
       console.log(error.message);
@@ -30,7 +24,7 @@ export class TaskformComponent implements OnInit {
   }
 
   updateTask(){
-    this.httService.put(this.url + this.child.task.taskId, this.child.task).subscribe(c => {
+    this.httService.put(this.child.url + this.child.task.taskId, this.child.task).subscribe(c => {
       this.child.getAllTask()
     })
   }
